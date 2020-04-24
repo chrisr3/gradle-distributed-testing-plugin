9a595fa5cb3dfeac52b803f76d1a590d13a745a0
Schife <razvan.codreanu@r3.com>
2020-04-24 13:59:22 +0100

    INFRA-284 removing unused imports

--- src/main/java/com/r3/testing/ImageBuilding.java
+++ src/main/java/com/r3/testing/ImageBuilding.java
@@ -7,8 +7,6 @@ import org.gradle.api.GradleException;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.Arrays;
