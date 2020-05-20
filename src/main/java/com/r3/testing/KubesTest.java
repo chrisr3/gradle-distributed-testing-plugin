4e1a48c126d43e4ef02119704c531b8fb0b27444
Euan Cairncross <euan.cairncross@r3.com>
2020-05-20 11:06:12 +0100

    INFRA-300 - Add Oracle

--- src/main/java/com/r3/testing/KubesTest.java
+++ src/main/java/com/r3/testing/KubesTest.java
@@ -423,7 +423,7 @@ public class KubesTest extends DefaultTask {
             addAdditionalArgsForAzureSQL();
             return buildPodRequestWithOnlyWorkerNode(podName, pvc, podIdx);
         } else if (withDB) {
-            // Postgres, MSSQL
+            // Postgres, MSSQL, Oracle
             return buildPodRequestWithWorkerNodeAndDBContainer(podName, pvc, podIdx);
         } else {
             // No DB / H2
